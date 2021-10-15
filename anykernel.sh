ffca3ab39d3cdad2bd9f3b67d42c9d6b7799bfb5
Nadins <77990703+nadinsylaa@users.noreply.github.com>
2021-10-15 08:56:09 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Styrofoam by NadinAlissa
+kernel.string=ThunderX by NadinAlissa × Meowhins
 do.devicecheck=1
 do.modules=0
 do.systemless=1
